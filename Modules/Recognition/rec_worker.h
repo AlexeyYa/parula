37839c4e79fca1736ddd722f7b2b848f53af29e5
Alexey <l337.cat@gmail.com>
2020-06-09 23:07:07 +0300

    ellipse recognition

--- Modules/Recognition/rec_worker.h
+++ Modules/Recognition/rec_worker.h
@@ -10,6 +10,7 @@
 #include "Modules/Recognition/prediction.h"
 #include "Modules/VectorGraphics/shapes.h"
 
+#include <dlib/matrix.h>
 #include <memory>
 #include <thread>
 
@@ -86,6 +87,7 @@ private:
             current = size - 1;
 
         }
+        PredictEllipse(0, m_data->stroke.size());
         if (m_prediction.active == RPrediction::Line)
         {
             std::cout << "Line through ";
@@ -105,7 +107,7 @@ private:
     }
 
     /*!
-     * \brief CheckLinePoints checks if all points are within line distance on indecies [first, last)
+     * \brief CheckLinePoints checks if all points are within distance from line for indecies [first, last)
      * \param first index
      * \param last index
      * \return check result
@@ -170,14 +172,136 @@ private:
 
     }
 
-    bool CheckEllipse(Shape::Ellipse ellipse, size_t first, size_t last)
+    /*!
+     * \brief CheckEllipsePoints checks if all points are within algebraic distance(a*x^2 + b*x*y + c*y^2 *d*x + e*y + f) on indecies [first, last)
+     * \param first index
+     * \param last index
+     * \return check result
+     * https://github.com/0xfaded/ellipse_demo/issues/1
+     */
+    bool CheckEllipsePoints(Shape::Ellipse ellipse, size_t first, size_t last)
     {
+        float maxd = 0;
+        for (size_t i = first; i < last; i++)
+        {
+            dlib::matrix<double, 2> p{ m_data->stroke[i].X - ellipse.center.X ,
+                                       m_data->stroke[i].Y - ellipse.center.Y };
+            auto s = sin(-ellipse.phi);
+            auto c = cos(-ellipse.phi);
+            dlib::matrix<double, 2, 2> rot2d{ c, -s , s, c };
+            auto p_rot = dlib::dot(rot2d, p);
+            auto p_abs = abs(p_rot);
 
+            dlib::matrix<double, 2> t{ 0.707 , 0.707 };
+            dlib::matrix<double, 2> ss{ ellipse.rl , ellipse.rs };
+            auto ss_sub = (ss * ss)(0) - (ss * ss)(1);
+
+            dlib::matrix<double, 2> efac{ ss_sub, -ss_sub };
+
+            for (int i = 0; i < 3; i++)
+            {
+                auto xy = ss * t;
+                auto e = efac * (t * t * t) / ss;
+                auto q = p_abs - e;
+                auto rq = dlib::norm(xy - e) / dlib::norm(q);
+            }
+
+            maxd = maxd > dist ? maxd : dist;
+            if (i % 20 == 0)
+                std::cout << dist << std::endl;
+            /*if (dist > m_threshold_distance)
+            {
+                return false;
+            }*/
+        }
+
+        std::cout << "maxdist: " << maxd << std::endl;
+        return true;
     }
 
+    /*!
+     * \brief PredictEllipse predict ellipse on indecies [first, last)
+     * \param first index
+     * \param last index
+     * \return true if got ellipse, else false
+     * http://www.users.on.net/~zygmunt.szpak/ellipsefitting.html
+     * src http://www.users.on.net/~zygmunt.szpak/sourcecode.html direct ellipse fit
+     */
     bool PredictEllipse(size_t first, size_t last)
     {
+        //std::vector<Shape::Point> points{ {0, 1}, {0, -1}, {-1, 0}, {1, 0} };
+        dlib::matrix<double> D1;
+        dlib::matrix<double> D2;
+        D1.set_size(last - first, 3);
+        D2.set_size(last - first, 3);
 
+        for (size_t i = first; i < last; i++)
+        {
+            D1(i, 0) = m_data->stroke[i].X * m_data->stroke[i].X;
+            D1(i, 1) = m_data->stroke[i].X * m_data->stroke[i].Y;
+            D1(i, 2) = m_data->stroke[i].Y * m_data->stroke[i].Y;
+            D2(i, 0) = m_data->stroke[i].X ;
+            D2(i, 1) = m_data->stroke[i].Y ;
+            D2(i, 2) = 1;
+        }
+        /*D1.set_size(4, 3);
+        D2.set_size(4, 3);
+
+        for (size_t i = 0; i < 4; i++)
+        {
+            D1(i, 0) = points[i].X * points[i].X;
+            D1(i, 1) = points[i].X * points[i].Y;
+            D1(i, 2) = points[i].Y * points[i].Y;
+            D2(i, 0) = points[i].X;
+            D2(i, 1) = points[i].Y;
+            D2(i, 2) = 1;
+        }*/
+
+        // Doesn't works well with auto for some reason
+        dlib::matrix<double> S1 = dlib::trans(D1) * D1;
+        dlib::matrix<double> S2 = dlib::trans(D1) * D2;
+        dlib::matrix<double> S3 = dlib::trans(D2) * D2;
+
+        dlib::matrix<double> invers3 = dlib::inv(S3);
+        dlib::matrix<double> trans2 = dlib::trans(S2);
+
+        dlib::matrix<double> T = - invers3 * trans2;
+        dlib::matrix<double> M = S1 + S2 * T;
+
+        dlib::matrix<double, 3, 3> M2;
+        dlib::set_rowm(M2, 0) = dlib::rowm(M, 2) / 2;
+        dlib::set_rowm(M2, 1) = - dlib::rowm(M, 1);
+        dlib::set_rowm(M2, 2) = dlib::rowm(M, 0) / 2;
+        
+        dlib::eigenvalue_decomposition<dlib::matrix<double>> eigen(M2);
+        auto evec = eigen.get_v();
+        dlib::matrix<double, 3, 1> al;
+        for (int i = 0; i < evec.nr(); i++)
+        {
+            auto cond = 4 * evec(0, i).real() * evec(2, i).real() - evec(1, i).real() * evec(1, i).real();
+            if (cond > 0)
+            {
+                al(0, 0) = evec(0, i).real();
+                al(1, 0) = evec(1, i).real();
+                al(2, 0) = evec(2, i).real();
+            }
+        }
+        dlib::matrix<double> tal = T * al;
+        dlib::matrix<double, 6, 1> a;
+        a(0) = al(0);
+        a(1) = al(1);
+        a(2) = al(2);
+        a(3) = tal(0);
+        a(4) = tal(1);
+        a(5) = tal(2);
+
+
+        a = dlib::normalize(a);
+
+        CheckEllipsePoints(Shape::Ellipse{ a(0), a(1), a(2), a(3), a(4), a(5) }, first, last);
+        
+
+        return true;
     }
 };
 
--- Modules/VectorGraphics/shapes.h
+++ Modules/VectorGraphics/shapes.h
@@ -3,11 +3,13 @@
 #define SHAPESTRUCT_H
 
 #include <memory>
+#include <math.h>
 
 namespace Shape
 {
     struct Point
     {
+        Point() : X(0), Y(0) {}
         Point(float X, float Y) : X(X), Y(Y) {}
 
         float X;
@@ -33,12 +35,31 @@ namespace Shape
 
     struct Ellipse : public Shape
     {
+        Ellipse(double a, double b, double c, double d, double e, double f) : a(a), b(b), c(c), d(d), e(e), f(f)
+        {
+            double tmp1 = b * b - 4 * a * c;
+            double tmp2 = sqrt((a - c) * (a - c) + b * b);
+            double tmp3 = a * e * e + c * d * d - b * d * e + tmp1 * f;
+            double r1 = sqrt(2 * tmp3 * (a + c + tmp2)) / tmp1;
+            double r2 = sqrt(2 * tmp3 * (a + c - tmp2)) / tmp1;
+
+            rl = r1 >= r2 ? r1 : r2;
+            rs = r1 <= r2 ? r1 : r2;
+
+            center.X = (2 * c * d - b * e) / tmp1;
+            center.Y = (2 * a * e - b * d) / tmp1;
+            
+            phi = 0.5 * atan2(b, a - c);
+            if (r1 > r2)
+                phi += 3.14159f / 2;
+        }
         // f(x, y) = a*x^2 + b*x*y + c*y^2 + d*x + e*y + f
-        float a, b, c, d, e, f;
+        double a, b, c, d, e, f;
+
         Point center;
-        float r1;
-        float r2;
-        float phi;
+        double rl;
+        double rs;
+        double phi;
     };
 
     //Container<std::shared_ptr<Shape>>
